f70f577ee814f4666b0082af8d7c951272d40fa3
tridoz <tridoz2905@gmail.com>
2026-07-18 08:25:39 +0200

    Modified stack_trace loggin with colors

--- src/Exceptions/cpp/HerionFileException.cpp
+++ src/Exceptions/cpp/HerionFileException.cpp
@@ -2,86 +2,46 @@
 
 #include <stack>
 
-
 // ================= FileException base =================
 HerionException::File::FileException::FileException(
-    const std::string& msg,
-    const std::string& type,
-    const std::string& location
+    const std::string& msg, const std::string& type, const std::string& location
 )
-    : std::runtime_error(msg),
-      type_(type),
-      location_(location)
-{
-    stack_trace_.insert(
-        stack_trace_.begin(),
-        location + " => " + msg
-    );
-
+    : std::runtime_error(msg), type_(type), location_(location) {
+    stack_trace_.insert(stack_trace_.begin(), location + " => " + msg);
 }
 
-std::vector < std::string > HerionException::File::FileException::GetStackTrace() const {
+std::vector<std::string> HerionException::File::FileException::GetStackTrace() const {
     return stack_trace_;
 }
 
-
 void HerionException::File::FileException::UpdateStackTrace(const std::string& stack_line) {
-    stack_trace_.insert(
-    stack_trace_.begin(),
-        stack_line
-    );
-
+    stack_trace_.insert(stack_trace_.begin(), stack_line);
 }
-
 
 // ================= FileNotFoundException =================
 HerionException::File::FileNotFoundException::FileNotFoundException(
-    const std::string& path,
-    const std::string& location
+    const std::string& path, const std::string& location
 )
-    : HerionException::File::FileException("File not found: " + path,
-                                           "FILE",
-                                           location)
-{}
-
+    : HerionException::File::FileException("\033[91mFile not found\033[0m: " + path, "FILE", location) {
+}
 
 // ================= FileOpenException =================
-HerionException::File::FileOpenException::FileOpenException(
-    const std::string& path,
-    const std::string& location
-)
-    : HerionException::File::FileException("Unable to open file: " + path,
-                                           "OPENING",
-                                           location)
-{}
-
+HerionException::File::FileOpenException::FileOpenException(const std::string& path, const std::string& location)
+    : HerionException::File::FileException("\033[91mUnable to open file\033[0m: " + path, "OPENING", location) {
+}
 
 // ================= FileReadException =================
-HerionException::File::FileReadException::FileReadException(
-    const std::string& path,
-    const std::string& location
-)
-    : HerionException::File::FileException("Unable to read file: " + path,
-                                           "READING",
-                                           location)
-{}
-
+HerionException::File::FileReadException::FileReadException(const std::string& path, const std::string& location)
+    : HerionException::File::FileException("\033[91mUnable to read file\033[0m: " + path, "READING", location) {
+}
 
 // ================= FileWriteException =================
-HerionException::File::FileWriteException::FileWriteException(
-    const std::string& path,
-    const std::string& location
-)
-    : HerionException::File::FileException("Unable to write file: " + path,
-                                           "WRITING",
-                                           location)
-{}
+HerionException::File::FileWriteException::FileWriteException(const std::string& path, const std::string& location)
+    : HerionException::File::FileException("\033[91mUnable to write file\033[0m: " + path, "WRITING", location) {
+}
 
 HerionException::File::FileMalformedException::FileMalformedException(
-    const std::string& path,
-    const std::string& location
+    const std::string& path, const std::string& location
 )
-    : HerionException::File::FileException("Malformed file: " + path,
-                                           "PARSING",
-                                           location)
-{}
+    : HerionException::File::FileException("\033[91mMalformed file\033[0m: " + path, "PARSING", location) {
+}
--- src/Exceptions/hpp/HerionFileException.hpp
+++ src/Exceptions/hpp/HerionFileException.hpp
@@ -1,101 +1,104 @@
 #pragma once
+#include <ctime>
 #include <stdexcept>
 #include <string>
-#include <ctime>
 
 #pragma once
 
 #if defined(_MSC_VER)
-    #define HERION_FUNCTION_SIGNATURE __FUNCSIG__
+#define HERION_FUNCTION_SIGNATURE __FUNCSIG__
 #elif defined(__GNUC__) || defined(__clang__)
-    #define HERION_FUNCTION_SIGNATURE __PRETTY_FUNCTION__
+#define HERION_FUNCTION_SIGNATURE __PRETTY_FUNCTION__
 #else
-    #define HERION_FUNCTION_SIGNATURE __FUNCTION__
+#define HERION_FUNCTION_SIGNATURE __FUNCTION__
 #endif
 
+#define HERION_RESET "\033[0m"
 
+#define HERION_BOLD "\033[1m"
+
+#define HERION_RED "\033[91m"
+#define HERION_YELLOW "\033[93m"
+#define HERION_CYAN "\033[96m"
 
 #include "../../Utils/hpp/Logger.hpp"
 
 namespace HerionException::File {
 
     class FileException : public std::runtime_error {
-    protected:
+      protected:
         std::string type_;
         std::string location_;
 
-        std::vector < std::string > stack_trace_;
-        std::vector < std::string > stack_trace_2;
+        std::vector<std::string> stack_trace_;
+        std::vector<std::string> stack_trace_2;
 
-
-    public:
+      public:
         explicit FileException(
-            const std::string& msg,
-            const std::string& type = "LOADING",
-            const std::string& location = "UnknownLocation"
+            const std::string& msg, const std::string& type = "LOADING", const std::string& location = "UnknownLocation"
         );
 
-        void UpdateStackTrace( const std::string& stack_line );
-        [[nodiscard]] std::vector< std::string > GetStackTrace() const;
-
+        void UpdateStackTrace(const std::string& stack_line);
+        [[nodiscard]] std::vector<std::string> GetStackTrace() const;
     };
 
     class FileNotFoundException final : public FileException {
-    public:
-        explicit FileNotFoundException(
-            const std::string& path,
-            const std::string& location = "UnknownLocation"
-        );
+      public:
+        explicit FileNotFoundException(const std::string& path, const std::string& location = "UnknownLocation");
     };
 
     class FileOpenException final : public FileException {
-    public:
-        explicit FileOpenException(
-            const std::string& path,
-            const std::string& location = "UnknownLocation"
-        );
+      public:
+        explicit FileOpenException(const std::string& path, const std::string& location = "UnknownLocation");
     };
 
     class FileReadException final : public FileException {
-    public:
-        explicit FileReadException(
-            const std::string& path,
-            const std::string& location = "UnknownLocation"
-        );
+      public:
+        explicit FileReadException(const std::string& path, const std::string& location = "UnknownLocation");
     };
 
     class FileWriteException final : public FileException {
-    public:
-        explicit FileWriteException(
-            const std::string& path,
-            const std::string& location = "UnknownLocation"
-        );
+      public:
+        explicit FileWriteException(const std::string& path, const std::string& location = "UnknownLocation");
     };
 
     class FileMalformedException final : public FileException {
-    public:
-        explicit FileMalformedException (
-            const std::string& path,
-            const std::string& location = "UnknownLocation"
-            );
+      public:
+        explicit FileMalformedException(const std::string& path, const std::string& location = "UnknownLocation");
     };
 
-}
+} // namespace HerionException::File
 
-#define THROW_FILE_NOT_FOUND(path) \
-    throw HerionException::File::FileNotFoundException( "\033[1m" + path + "\033[22m", HERION_FUNCTION_SIGNATURE)
+#define THROW_FILE_NOT_FOUND(path)                                                                                     \
+    throw HerionException::File::FileNotFoundException(                                                                \
+        std::string(HERION_RED) + HERION_BOLD + (path) + HERION_RESET,                                                 \
+        std::string(HERION_CYAN) + HERION_FUNCTION_SIGNATURE + HERION_RESET                                            \
+    )
 
-#define THROW_FILE_OPEN(path) \
-    throw HerionException::File::FileOpenException( "\033[1m" + path + "\033[22m", HERION_FUNCTION_SIGNATURE)
+#define THROW_FILE_OPEN(path)                                                                                          \
+    throw HerionException::File::FileOpenException(                                                                    \
+        std::string(HERION_RED) + HERION_BOLD + (path) + HERION_RESET,                                                 \
+        std::string(HERION_CYAN) + HERION_FUNCTION_SIGNATURE + HERION_RESET                                            \
+    )
 
-#define THROW_FILE_READ(path) \
-throw HerionException::File::FileReadException( "\033[1m" + path + "\033[22m", HERION_FUNCTION_SIGNATURE)
+#define THROW_FILE_READ(path)                                                                                          \
+    throw HerionException::File::FileReadException(                                                                    \
+        std::string(HERION_RED) + HERION_BOLD + (path) + HERION_RESET,                                                 \
+        std::string(HERION_CYAN) + HERION_FUNCTION_SIGNATURE + HERION_RESET                                            \
+    )
 
-#define THROW_FILE_WRITE(path) \
-    throw HerionException::File::FileWriteException( "\033[1m" + path + "\033[22m", HERION_FUNCTION_SIGNATURE)
+#define THROW_FILE_WRITE(path)                                                                                         \
+    throw HerionException::File::FileWriteException(                                                                   \
+        std::string(HERION_RED) + HERION_BOLD + (path) + HERION_RESET,                                                 \
+        std::string(HERION_CYAN) + HERION_FUNCTION_SIGNATURE + HERION_RESET                                            \
+    )
 
-#define THROW_FILE_MALFORMED(path) \
-    throw HerionException::File::FileMalformedException( "\033[1m" + path + "\033[22m", HERION_FUNCTION_SIGNATURE)
+#define THROW_FILE_MALFORMED(path)                                                                                     \
+    throw HerionException::File::FileMalformedException(                                                               \
+        std::string(HERION_RED) + HERION_BOLD + (path) + HERION_RESET,                                                 \
+        std::string(HERION_CYAN) + HERION_FUNCTION_SIGNATURE + HERION_RESET                                            \
+    )
 
-#define GET_CONTEXT() \
-    (std::string(HERION_FUNCTION_SIGNATURE) + " at line => " + std::to_string(__LINE__))
+#define GET_CONTEXT()                                                                                                  \
+    (std::string(HERION_CYAN) + HERION_FUNCTION_SIGNATURE + HERION_RESET + " | " +                                     \
+     HERION_YELLOW "Line: " + std::to_string(__LINE__) + HERION_RESET)
